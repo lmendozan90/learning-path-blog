71cc0121cd8bef096c378936ab029ecbe9e52bd1
lmendozan90 <lmendozan90@gmail.com>
2025-04-09 18:30:14 -0500

    Adding a create BlogPost action

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -11,6 +11,20 @@ class BlogPostsController < ApplicationController
 
   def new
     @blog_post = BlogPost.new
-    
+  end
+
+  def create
+    @blog_post = BlogPost.new(blog_post_params)
+    @blog_post.save
+    if @blog_post.save
+      redirect_to @blog_post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def blog_post_params
+    params.require(:blog_post).permit(:title, :body)
   end
 end
