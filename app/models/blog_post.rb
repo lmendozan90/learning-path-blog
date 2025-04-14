d6b15f015c4b4ca1722bb2feae45b1e24ca73f3e
lmendozan90 <lmendozan90@gmail.com>
2025-04-14 15:30:54 -0500

    Adding Scheduled posts

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,22 @@
 class BlogPost < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
+
+  scope :sorted, -> { order("published_at DESC NULLS LAST, updated_at DESC") }
+  scope :draft, -> { where(published_at: nil) }
+  scope :published, -> { where("published_at <= ?", Time.current) }
+  scope :scheduled, -> { where("published_at > ?", Time.current) }
+
+  def draft?
+    published_at.nil?
+  end
+
+  def published?
+    published_at.present? && published_at <= Time.current
+  end
+
+  def scheduled?
+    published_at.present? && published_at > Time.current
+  end
 end
+
